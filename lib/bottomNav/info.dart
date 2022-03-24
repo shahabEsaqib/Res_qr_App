0832f631ce459b45c4d2b998ee64b60c6099733b
shahabEsaqib <shahab.sk708@gmail.com>
2022-03-24 10:04:36 +0500

    final committt

--- lib/bottomNav/info.dart
+++ lib/bottomNav/info.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
-class InfoScreen extends StatelessWidget {
+class InfoScreen extends StatefulWidget {
   const InfoScreen({ Key? key }) : super(key: key);
 
+  @override
+  State<InfoScreen> createState() => _InfoScreenState();
+}
+
+class _InfoScreenState extends State<InfoScreen> {
+  TextStyle myStyle = TextStyle(fontSize: 20,color: Colors.red);
+  TextStyle myBoldStyle = TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -53,9 +61,14 @@ class InfoScreen extends StatelessWidget {
                   leading: Text('Intake Forms',style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),),
                 ),
               ),
-              Card(
-                child: ListTile(
-                  leading: Text('Disclaimer',style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),),
+              GestureDetector(
+                onTap: (){
+                  showMyDialog();
+                },
+                child: Card(
+                  child: ListTile(
+                    leading: Text('Disclaimer',style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),),
+                  ),
                 ),
               ),
               Card(
@@ -81,4 +94,42 @@ class InfoScreen extends StatelessWidget {
       ),
     );
   }
+
+  Future<void> showMyDialog() async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Center(child: Text('WARNING!',style: myStyle,)),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Center(child: Text('In any medical-related emergency within the United State dial 911.',style: myBoldStyle,)),
+              SizedBox(height: 2.h,),
+              Center(child: Text('Always check the scene to be sure it is safe to aproach and helo others.',style: myBoldStyle,)),
+              SizedBox(height: 2.h,),
+              Center(child: Text('DECISION CONCERNING CPR & AED USE ARE LIFE AND DEATH DECISIONS THAT MAY INVOLVE RISK OF SERIOUS INJURY OR DEATH.',style: myBoldStyle,)),
+              SizedBox(height: 2.h,),
+              Center(child: Text('if you suspect serious injury may result to you personally, do not aproach and call 911 or seek other emergency designated care assistance.',style: myBoldStyle,)),
+              SizedBox(height: 2.h,),
+              Center(child: Text('Thoroughly fimiliarize yourself with this mobile application and its operation.',style: myBoldStyle,)),
+
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          Center(
+            child: TextButton(
+              child: Text('OK',style: TextStyle(color: Colors.blue,fontSize: 20)),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
 }
